b5ed6ccbb784c24cdab25d5f0709bf7089f67072
Hongyi Guo <guohongyi1991@gmail.com>
2016-04-17 19:13:15 -0500

    mainly done

---
+++ Build/Intermediates/UDPClient.build/Debug-iphoneos/GPSCar.build/Objects-normal/arm64/IPAddress-75113B2A1DCF0787.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/Tibbers/Google\ Drive/2016Spring/RealTimeSystems/project/udpclient_swift-master/UDPClient/ysocket/IPAddress.m \
+  /Users/Tibbers/Google\ Drive/2016Spring/RealTimeSystems/project/udpclient_swift-master/UDPClient/ysocket/IPAddress.h
---
+++ Build/Intermediates/UDPClient.build/Debug-iphoneos/GPSCar.build/Objects-normal/arm64/ytcpsocket.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/Tibbers/Google\ Drive/2016Spring/RealTimeSystems/project/udpclient_swift-master/UDPClient/ysocket/ytcpsocket.c
