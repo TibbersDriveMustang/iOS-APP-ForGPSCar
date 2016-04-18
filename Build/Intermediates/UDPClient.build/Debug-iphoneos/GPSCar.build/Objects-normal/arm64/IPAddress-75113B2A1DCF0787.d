f7f3dd708884be4a2ec4c89860afab88f9887354
Hongyi Guo <guohongyi1991@gmail.com>
2016-04-17 19:15:50 -0500

    Revert "mainly done"
    This reverts commit b5ed6ccbb784c24cdab25d5f0709bf7089f67072.

--- Build/Intermediates/UDPClient.build/Debug-iphoneos/GPSCar.build/Objects-normal/arm64/IPAddress-75113B2A1DCF0787.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Tibbers/Google\ Drive/2016Spring/RealTimeSystems/project/udpclient_swift-master/UDPClient/ysocket/IPAddress.m \
-  /Users/Tibbers/Google\ Drive/2016Spring/RealTimeSystems/project/udpclient_swift-master/UDPClient/ysocket/IPAddress.h
--- Build/Intermediates/UDPClient.build/Debug-iphoneos/GPSCar.build/Objects-normal/arm64/ytcpsocket.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/Tibbers/Google\ Drive/2016Spring/RealTimeSystems/project/udpclient_swift-master/UDPClient/ysocket/ytcpsocket.c
